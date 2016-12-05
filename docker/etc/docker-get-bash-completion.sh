3f6bb89cd3e21d71577cf22e906103c171ffa1cc
bo <bo@e121.suse.de>
2016-12-05 13:17:31 +0100

    Add autocompletion for docker-get-bash

---
+++ docker/etc/docker-get-bash-completion.sh
@@ -0,0 +1,12 @@
+_script()
+{
+  _script_commands=$(docker ps --format '{{.Names}}')
+
+  local cur prev
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=( $(compgen -W "${_script_commands}" -- ${cur}) )
+
+  return 0
+}
+complete -o nospace -F _script docker-get-bash
